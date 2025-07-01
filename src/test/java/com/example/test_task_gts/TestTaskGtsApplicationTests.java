d7bb6434abde66bb860616f0bdd9f8beb1903d38
elnuraamantur22 <elnura.amantur@gmail.com>
2025-07-01 15:44:46 +0600

    add readme.md

---
+++ src/test/java/com/example/test_task_gts/TestTaskGtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_task_gts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskGtsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
